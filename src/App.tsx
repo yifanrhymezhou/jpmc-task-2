a287710db61b8eca7686d4cb40ce2f4080e8dc9f
Yifan (Rhyme) Zhou <107736520+yifanrhymezhou@users.noreply.github.com>
2023-02-26 11:37:10 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,12 @@ import './App.css';
 /**
  * State declaration for <App />
  */
+/**
+ * Whenever a type of ISate is used, it should always have data and showGraph
+ */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -22,26 +26,38 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
 
   /**
-   * Render Graph react component with state.data parse as property data
+   * When users click 'start', render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
-  }
+    if (this.state.showGraph) {
+    return (<Graph data={this.state.data}/>);
+  };
+}
 
-  /**
-   * Get new data from server and update the state with the new data
-   */
+    /**
+     * Get new data from server and update the state with the new data
+     */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
-  }
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x>1000){
+        clearInterval(interval);
+      }
+    }, 10);
+  };
+
 
   /**
    * Render the App react component
@@ -72,3 +88,6 @@ class App extends Component<{}, IState> {
 }
 
 export default App;
+function getDataFromServer() {
+  throw new Error('Function not implemented.');
+}
